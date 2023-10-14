d0c2b27f383b18de18ae89e52ff276b4ebcc9f5c
beiju <beiju@me.com>
2023-10-14 02:56:09 -0400

    Committing debugging code to see if vercel is doing wierd stuff with my database

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,7 +4,9 @@ import { VersionSelect } from "@/components/Universe"
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { rows } = await sql`SELECT data, id FROM universes where id = ${params.id};`
+  console.log("Got", rows.length, "results")
   const rawUniverse: RawUniverse = JSON.parse(rows[0].data)
+  console.log("Read universe from db at tick", rawUniverse.sim.state.tick, "with id", params.id)
   return (
     <VersionSelect universe={rawUniverse} universeId={rows[0].id} />
   )
--- src/app/api/universe/[universeId]/route.ts
+++ src/app/api/universe/[universeId]/route.ts
@@ -1,6 +1,7 @@
 import { sql } from "@vercel/postgres"
 import { NextResponse } from "next/server"
 import { CHECKPOINT_INTERVAL, CHECKPOINT_TIME_CACHE } from "@/app/api/updateTimeCache"
+import { RawUniverse } from "@/sim/sim"
 
 export async function PUT(req: Request, { params }: { params: { universeId: string } }) {
   const data = await req.text()
@@ -14,7 +15,15 @@ export async function PUT(req: Request, { params }: { params: { universeId: stri
       status: "skipped",
     })
   }
-  await sql`UPDATE universes SET data = ${data} WHERE id = ${universeId};`
+  console.log("Writing universe to db at tick", JSON.parse(data).sim.state.tick, "with id", universeId)
+  const { rows } = await sql`UPDATE universes SET data = ${data} WHERE id = ${universeId} RETURNING data;`
+  console.log("db call returned tick", JSON.parse(rows[0].data).sim.state.tick)
+
+  const { rows: r } = await sql`SELECT data, id FROM universes where id = ${universeId};`
+  console.log("Got", r.length, "results")
+  const rawUniverse: RawUniverse = JSON.parse(r[0].data)
+  console.log("Read universe from db at tick", rawUniverse.sim.state.tick, "with id", universeId)
+
   CHECKPOINT_TIME_CACHE[universeId] = now
   return NextResponse.json({
     status: "success",
